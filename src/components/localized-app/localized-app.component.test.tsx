1a5ca213c1320079f32a117efef403421b919f23
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2022-12-13 22:31:56 +0000

    VBF-22 new: begin implementing unit test for localized-app component but blocked by no routing unit tests

---
+++ src/components/localized-app/localized-app.component.test.tsx
@@ -0,0 +1,45 @@
+import { render } from "@testing-library/react"
+import React from "react"
+import LocalizedAppComponent from './localized-app.component'
+import Cookies from "universal-cookie"
+
+jest.mock('../../localization/en.json', () => {return {"test": "test"}}, { virtual: true })
+jest.mock('../../localization/hi.json', () => {return {"test": "test"}}, { virtual: true })
+jest.mock('../../localization/vi.json', () => {return {"test": "test"}}, { virtual: true })
+
+jest.mock("axios", () => {
+    return {
+        defaults: {
+            headers: {
+                common: {
+                    Authorization: ""
+                }
+            }
+        }
+    }
+})
+
+jest.mock("universal-cookie", () => {
+    const mCookie = {
+        get: jest.fn()
+    }
+
+    return jest.fn(() => mCookie);
+})
+
+const cookies = new Cookies()
+
+describe('LocalizedAppComponent', () => {
+
+    //TBD when routing unit tests implemented
+
+    test('LocalizedAppComponent should exist', () => {
+        
+        // (cookies.get as jest.Mocked<any>).mockReturnValueOnce({
+        //     value: 'en'
+        // })
+
+        // let { container } = render(<LocalizedAppComponent lang='en' />)
+        // expect(container).toBeInTheDocument()
+    })
+})
