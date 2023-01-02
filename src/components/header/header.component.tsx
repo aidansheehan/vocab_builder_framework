180cf8c3c531e195353f43a96cb6768d715f030f
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-01-02 18:39:15 +0000

    VBF-20 new: basic header implementation for desktop, translations added, flex layout applied, colors as in wireframes

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -2,6 +2,7 @@ import { NavLink }                      from 'react-router-dom';
 import useAppDispatch                   from '../../hooks/redux/use-app-dispatch.hook';
 import useAppSelector                   from '../../hooks/redux/use-app-selector.hook';
 import { logout }                       from '../../redux/features/user/user.slice';
+import ButtonComponent                  from '../button/button.component';
 import LocaleSelectorComponent          from '../locale-selector/locale-selector.component';
 import TextComponent                    from '../text/text.component';
 import styles                           from './header.component.scss';
@@ -24,38 +25,47 @@ const HeaderComponent = (): JSX.Element => {
 
     return (
             <div className={styles.header}>
-                <span>
-                    {userInfo ? `Logged in as ${userInfo.username}` : 'You\'re not logged in'}
-                </span>
 
-                <nav>
-                    {userInfo ? (
-                        <div>
-                            {/* TODO this should be generic button component */}
-                            <button role='nav-link' data-testid='logout-link' onClick={() => dispatch(logout())}>
-                                <TextComponent textRef='nav_logout_link_title' />
-                            </button>
-                            <NavLink role='nav-link' data-testid='collections-link' to={`/collections`}>
-                                <TextComponent textRef='nav_collections_link_title' />
-                            </NavLink>
-                        </div>
-                    ) : (
-                        <div>
-                            <NavLink role='nav-link' data-testid='landing-link' to={`/`} >
-                                <TextComponent textRef='nav_home_link_title' />
-                            </NavLink>
-                            <NavLink role='nav-link' data-testid='login-link' to={`/login`}>
-                                <TextComponent textRef='nav_login_link_title' />
-                            </NavLink>
-                            <NavLink role='nav-link' data-testid='register-link' to={`/register`}>
-                                <TextComponent textRef='nav_register_link_title' />
-                            </NavLink>
-                        </div>
-                    )}
+                <div className={styles.headerLeft} >
+                    <span className={styles.logo}>VOCAB BUILDER</span>
+                    <LocaleSelectorComponent />
+                </div>
 
-                </nav>
-                
-                <LocaleSelectorComponent />
+
+                <div className={styles.headerRight} >
+
+                    <nav>
+                        {userInfo ? (
+                            <>
+                                <NavLink role='nav-link' data-testid='collections-link' to={`/collections`}>
+                                    <TextComponent textRef='nav_collections_link' />
+                                </NavLink>
+                                <NavLink role='nav-link' to={'/collections/new'}>
+                                    <TextComponent textRef='nav_new-collection_link' />
+                                </NavLink>
+                                <ButtonComponent
+                                    textRef='nav_logout_link'
+                                    onClick={() => dispatch(logout())}
+                                    style={styles.logoutButton}
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <NavLink role='nav-link' data-testid='landing-link' to={`/`} >
+                                    <TextComponent textRef='nav_home_link' />
+                                </NavLink>
+                                <NavLink role='nav-link' data-testid='login-link' to={`/login`}>
+                                    <TextComponent textRef='nav_login_link' />
+                                </NavLink>
+                                <NavLink role='nav-link' data-testid='register-link' to={`/register`}>
+                                    <TextComponent textRef='nav_register_link' />
+                                </NavLink>
+                            </>
+                        )}
+
+                    </nav>
+
+                </div>
             </div>
     )
 }
