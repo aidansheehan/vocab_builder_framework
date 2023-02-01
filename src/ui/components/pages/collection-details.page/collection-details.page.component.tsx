a5f58d3319851cd072c152be141d0f4d6962629e
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-02-01 21:59:15 +0000

    VBF-28 new: update routes so they use a url param for collection ID, implement collection details page functionality and responsive styling

--- src/ui/components/pages/collection-details.page/collection-details.page.component.tsx
+++ src/ui/components/pages/collection-details.page/collection-details.page.component.tsx
@@ -1,3 +1,12 @@
+import CollectionDetailsCardDisplayComponent    from './components/collection-details.page.card-display/collection-details.page.card-display.component'
+import styles                                   from './collection-details.page.component.scss'
+import TextValueComponent                       from '../../text-value/text-value.component'
+import ButtonPrimaryComponent                   from '../../button/components/button-primary.component'
+import useAppSelector                           from '../../../hooks/redux/use-app-selector.hook'
+import useAppDispatch                           from '../../../hooks/redux/use-app-dispatch.hook'
+import { useEffect }                            from 'react'
+import { getOneCollection }                     from '../../../../redux/collections/actions/collections.actions'
+
 /**
  * Collection Details Page
  * @author Aidan Sheehan <aidanmsheehan@gmail.com>
@@ -9,12 +18,78 @@
  */
 const CollectionDetailsPageComponent = (): JSX.Element => {
 
-    //Todo this page HAS to have a collection ID
+    const params        = new URLSearchParams(window.location.search)   //Get url search params
+    const collectionId  = params.get('collectionId')                    //Get collectionId from params
+
+    const dispatch = useAppDispatch()
+
+    if (!collectionId) {
+        //TODO handle case no collection ID
+        console.error('no collection ID provided')
+    }
+
+
+    //Attempt to retrieve collection from redux store
+    const collection = useAppSelector(state => state.collections.collections[collectionId])
+
+    //ComponentDidMount
+    useEffect(() => {
+
+        if (!collection) {
+            //Function to make a GET request to retrieve the collection from the backend
+            const retrieveCollection = async () => {
+
+                //Dispatch action to retrieve the collection
+                await dispatch(getOneCollection(collectionId))
+            }
+
+            //Execute function to retrieve collection
+            retrieveCollection()
+
+        }
+
+    }, [])
 
     return (
-        <div>
-            I am the Colletion Details Page.
-        </div>
+        <>
+            {
+                collection
+                ?
+                <div className={styles.collectionDetails}>
+
+                    <div className={styles.topPanel}>
+        
+                        <div className={styles.collectionInfo} >
+        
+                            <div className={styles.title} >
+                                <TextValueComponent value={collection.title} />
+                            </div>
+        
+                            <div className={styles.description}>
+                                <TextValueComponent value={collection.description} />
+                            </div>
+        
+                        </div>
+        
+                        <div className={styles.collectionButtons} >
+                            <ButtonPrimaryComponent textRef='collection-details_nav_play' onClick={() => alert('play game')} />
+                            <ButtonPrimaryComponent textRef='collection-details_nav_edit' onClick={() => alert('edit')} style={styles.editBtn} />
+                        </div>
+        
+                    </div>
+        
+                    {
+                        collection.cards.map((c_, i_) => (
+                            <CollectionDetailsCardDisplayComponent card={c_} key={i_} />
+                        ))
+                    }
+        
+                </div>
+                :
+                <div>LOADING...</div>
+            }
+        </>
+
     )
 }
 
---
+++ src/ui/components/pages/collection-details.page/components/collection-details.page.card-display/collection-details.page.card-display.component.tsx
@@ -0,0 +1,46 @@
+import classNames           from 'classnames'
+import { CardType }         from '../../../../../../redux/collections/types/collections.types'
+import TextValueComponent   from '../../../../text-value/text-value.component'
+import styles               from './collection-details.page.card-display.component.scss'
+
+/** CollectionDetailsCardDisplayComponentProps type */
+type CollectionDetailsCardDisplayComponentProps = {
+
+    /** The Card to be displayed */
+    card: CardType
+
+}
+
+/**
+ * Component to display a single collection card
+ * @author Aidan Sheehan <aidanmsheehan@gmail.com>
+ * @version 0.1.0
+ * @component
+ * @example
+ * let card = { lexi: "Billi", description: "Cat" } 
+ * return (
+ *   <CollectionDetailsCardDisplayComponent card={card} />
+ * )
+ */
+const CollectionDetailsCardDisplayComponent = (props: CollectionDetailsCardDisplayComponentProps): JSX.Element => {
+
+    const { card }                  = props     //Destructure props
+    const { lexi, description }     = card      //Destructure card
+
+    return (
+        <div className={styles.card} >
+
+            <div className={classNames(styles.cardLexi, styles.cardText)} >
+                <TextValueComponent value={lexi} />
+            </div>
+
+            {/* TODO this will need to be refactored to loop through an array of 'prompts' */}
+            <div className={classNames(styles.cardDescription, styles.cardText)} >
+                <TextValueComponent value={description} />
+            </div>
+
+        </div>
+    )
+}
+
+export default CollectionDetailsCardDisplayComponent
