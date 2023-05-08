bdba20f1edc59e03068cc46167413fdc603dda1b
Aidan Sheehan <aidansheehan@Aidans-MacBook-Air.local>
2023-05-08 09:54:02 +0100

    VBF-91 new: random game finish celebration messages, game finish page styles, game finish page jsx

--- src/ui/pages/game-finish.page/game-finish.page.tsx
+++ src/ui/pages/game-finish.page/game-finish.page.tsx
@@ -1,3 +1,8 @@
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import ButtonComponent from '../../components/button/button.component'
+import TextComponent from '../../components/text/text.component'
+import styles from './game-finish.page.scss'
+
 /**
  * Game finish page
  * @author Aidan Sheehan <aidanmsheehan@gmail.com>
@@ -10,9 +15,30 @@
  */
 const GameFinishPage = (): JSX.Element => {
 
+    const randFinishMessage = Math.floor(Math.random() * 10) + 1
+
+    const navigate          = useNavigate()
+    const [ searchParams ]  = useSearchParams()
+
+    const gameStartPath = `/user/collection/play${searchParams ? '?' + searchParams.toString() : ''}`
+
     return (
-        <div>
-            I am the game finish page.
+        <div className={styles.gameFinishPage} >
+
+            <div className={styles.gameFinishContent} >
+
+                <TextComponent title textRef={`game-finish_message_${randFinishMessage}`} style={styles.gameFinishMessage} />
+
+                <div className={styles.gameFinishControl} >
+
+                    <ButtonComponent primary textRef='game-finish_play-again' onClick={() => navigate(gameStartPath)} icon='rotate-left' />
+                    <ButtonComponent secondary textRef='nav_home_link' onClick={() => navigate('/user')} icon='house' />
+
+                </div>
+                
+
+            </div>
+
         </div>
     )
 }
